9ea75ffa782c6410615507ea5719e92fdfbb70fd
ishiy <y.ishihara.1993@gmail.com>
2017-01-27 15:10:20 +0900

    Refactoring

--- src/Network/Google/OAuth2.hs
+++ src/Network/Google/OAuth2.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Network.Google.OAuth2 where
+module Network.Google.OAuth2
+    ( getToken
+    , OAuth2Client(..)
+    , Scope
+    , AccessToken
+    ) where
 
 import Control.Concurrent
 import Control.Exception (throwIO)
@@ -12,6 +18,7 @@ import Network.HTTP.Req
 import Network.Wai
 import Network.Wai.Handler.Warp
 import System.Directory
+import System.Exit
 import System.FilePath
 
 getToken :: OAuth2Client -> FilePath -> [Scope] -> IO AccessToken
@@ -21,78 +28,83 @@ getToken c tokenFile scopes = do
 
 readToken :: OAuth2Client -> FilePath -> IO AccessToken
 readToken c tokenFile = do
-    token <- read <$> readFile tokenFile
-    let e = fromIntegral $ expiresIn token - 5
+    t <- read <$> readFile tokenFile
+    let e = fromIntegral $ expiresIn t - 5
     now <- getCurrentTime
-    t <- getModificationTime tokenFile
-    if now < addUTCTime e t
-       then return $ B.pack $ accessToken token
-       else getNewToken c tokenFile (refreshToken token)
+    mt <- getModificationTime tokenFile
+    if now < addUTCTime e mt
+       then return $ B.pack $ accessToken t
+       else do
+           t' <- getNewTokenInfo c (refreshToken t)
+           saveTokenInfo tokenFile t'
+           return $ B.pack $ accessToken t'
 
-getNewToken :: OAuth2Client -> FilePath -> RefreshToken -> IO AccessToken
-getNewToken c tokenFile rt = do
+getNewTokenInfo :: OAuth2Client -> RefreshToken -> IO TokenInfo
+getNewTokenInfo c rt = do
     let body = ReqBodyUrlEnc $
                    "refresh_token" =: rt <>
                    "client_id" =: clientId c <>
                    "client_secret" =: clientSecret c <>
                    "grant_type" =: ("refresh_token" :: String)
-        Just (url, opt) = parseUrlHttps tokenUri
-    res <- req POST url body jsonResponse opt
-    let t' = responseBody res :: TokenInfo
-        t = t' { refreshToken = rt }
+    res <- req POST tokenUrl body jsonResponse mempty
+    let t' = responseBody res
+    return $ t' { refreshToken = rt }
+
+saveTokenInfo :: FilePath -> TokenInfo -> IO ()
+saveTokenInfo tokenFile t = do
+    createDirectoryIfMissing True $ takeDirectory tokenFile
     writeFile tokenFile (show t)
-    return $ B.pack $ accessToken t
-
-tokenUri = "https://accounts.google.com/o/oauth2/token"
-
-serverPort :: Port
-serverPort = 8017
 
 downloadToken :: OAuth2Client -> FilePath -> [Scope] -> IO AccessToken
 downloadToken c tokenFile scopes = do
+    code <- getCode c scopes
+    t <- exchangeCode c code
+    saveTokenInfo tokenFile t
+    return $ B.pack $ accessToken t
+
+getCode :: OAuth2Client -> [Scope] -> IO Code
+getCode c scopes = do
     let authUri = "https://accounts.google.com/o/oauth2/v2/auth"
-        redirectUri = "http://127.0.0.1:" ++ show serverPort
         q = renderSimpleQuery True
                 [ ("scope", B.pack $ unwords scopes)
                 , ("redirect_uri", B.pack redirectUri)
                 , ("response_type", "code")
                 , ("client_id", B.pack $ clientId c)
                 ]
-    -- get code
     putStrLn "Open the following uri in your browser:"
     putStrLn $ B.unpack $ authUri <> q
     m <- newEmptyMVar
-    _ <- forkIO $ startServer m
-    code <- takeMVar m
+    _ <- forkIO $ run serverPort (app m)
+    takeMVar m
 
-    -- exchange code
-    let Just (url, opt) = parseUrlHttps tokenUri
-        body = ReqBodyUrlEnc $
+app :: MVar Code -> Application
+app m request respond = do
+    case lookup "code" (queryString request) of
+         Just (Just code) -> putMVar m $ B.unpack code
+         _ -> die "Unable to get code"
+    respond $ responseLBS status200
+                          [("Content-Type", "text/plain")]
+                          "Return your app"
+
+exchangeCode :: OAuth2Client -> Code -> IO TokenInfo
+exchangeCode c code = do
+    let body = ReqBodyUrlEnc $
                    "code" =: code <>
                    "client_id" =: clientId c <>
                    "client_secret" =: clientSecret c <>
                    "redirect_uri" =: redirectUri <>
                    "grant_type" =: ("authorization_code" :: String)
-    res <- req POST url body jsonResponse opt
-    let t = responseBody res :: TokenInfo
-    createDirectoryIfMissing True $ takeDirectory tokenFile
-    writeFile tokenFile (show t)
-    putStrLn $ "Save token in " ++ tokenFile
+    res <- req POST tokenUrl body jsonResponse mempty
+    return $ responseBody res
 
-    return $ B.pack $ accessToken t
+tokenUrl :: Url 'Https
+tokenUrl = https "accounts.google.com" /: "o" /: "oauth2" /: "token"
 
-startServer :: MVar Code -> IO ()
-startServer m = run serverPort (app m)
+serverPort :: Port
+serverPort = 8017
 
-app :: MVar Code -> Application
-app m request respond = do
-    print request
-    case lookup "code" (queryString request) of
-         Just (Just code) -> putMVar m $ B.unpack code
-         _ -> return ()
-    respond $ responseLBS status200
-                          [("Content-Type", "text/plain")]
-                          "Return your app"
+redirectUri :: String
+redirectUri = "http://127.0.0.1:" ++ show serverPort
 
 data OAuth2Client = OAuth2Client
     { clientId :: String
@@ -113,7 +125,7 @@ data TokenInfo = TokenInfo
 
 instance FromJSON TokenInfo where
     parseJSON (Object v) = TokenInfo <$> v .: "access_token"
-                                     <*> (v .:? "refresh_token" .!= "")
+                                     <*> v .:? "refresh_token" .!= ""
                                      <*> v .: "expires_in"
     parseJSON _ = mempty
 
